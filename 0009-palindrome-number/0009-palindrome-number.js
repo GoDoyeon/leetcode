3bd3d021578287c5faf9a94c4b02b2cf5b626508
고도연 <69631850+GoDoyeon@users.noreply.github.com>
2023-03-19 14:38:54 +0900

    Time: 202 ms (23.69%), Space: 52 MB (9.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,7 +6,7 @@ var isPalindrome = function (x) {
   if (x < 0) return false;
   let arr = Array.from(String(x));
 
-  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+  for (let i = 0; i < arr.length / 2; i++) {
     if (arr[i] !== arr[arr.length - 1 - i]) {
       return false;
     }
