30589ac00c38f0e120221a60f76161d466c11e53
고도연 <69631850+GoDoyeon@users.noreply.github.com>
2023-04-30 00:12:14 +0900

    Time: 56 ms (71.90%), Space: 42 MB (59.03%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -3,7 +3,5 @@
  * @return {string}
  */
 var interpret = function(command) {
-    command = command.replaceAll('()', 'o')
-    command = command.replaceAll('(al)', 'al')
-    return command
+    return command.replaceAll('()','o').replaceAll('(al)','al')
 };
